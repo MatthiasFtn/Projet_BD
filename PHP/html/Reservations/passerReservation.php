de8e8d36c381a648a9b0892376de0a4ba3048ee2
Matthias <mattfon23@gmail.com>
2021-04-12 10:22:44 +0200

    Add activites

--- PHP/html/Reservations/passerReservation.php
+++ PHP/html/Reservations/passerReservation.php
@@ -1,4 +1,4 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
@@ -238,93 +238,5 @@
         </div>
     </form>
   
-</body>
-</html> -->
-
-
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1"/>
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
-	</head>
-<body>
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<a class="navbar-brand" href="https://sourcecodester.com">Sourcecodester</a>
-		</div>
-	</nav>
-	<div class="col-md-3"></div>
-	<div class="col-md-6 well">
-		<h3 class="text-primary">PHP - Insert Data to SQLite Using PDO</h3>
-		<hr style="border-top:1px dotted #ccc;"/>
-		<button type="button" class="btn btn-success" data-toggle="modal" data-target="#form_modal"><span class="glyphicon glyphicon-plus"></span> Add Member</button>
-		<br /><br />
-		<table class="table table-bordered">
-			<thead class="alert-info">
-				<tr>
-					<th>Firstname</th>
-					<th>Lastname</th>
-					<th>Address</th>
-				</tr>
-			</thead>
-			<tbody style="background-color:#fff;">
-				<?php
-					require 'conn.php';
-					
-					$query = $conn->prepare("SELECT * FROM `member` ORDER BY `lastname` ASC");
-					$query->execute();
-					while($fetch = $query->fetch()){
-				?>
-				<tr>
-					<td><?php echo $fetch['firstname']?></td>
-					<td><?php echo $fetch['lastname']?></td>
-					<td><?php echo $fetch['address']?></td>
-				</tr>
-				<?php
-					}
-					
-					$conn = null;
-				?>
-			</tbody>
-		</table>
-	</div>
-	<div class="modal fade" id="form_modal" aria-hidden="true">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<form method="POST" action="save_member.php">
-					<div class="modal-header">
-						<div class="modal-title">
-							<h3>Add Member</h3>
-						</div>
-					</div>
-					<div class="modal-body">
-						<div class="col-md-2"></div>
-						<div class="col-md-8">
-							<div class="form-group">
-								<label>Firstname</label>
-								<input type="text" name="firstname" class="form-control" required="required"/>
-							</div>
-							<div class="form-group">
-								<label>Lastname</label>
-								<input type="text" name="lastname" class="form-control" required="required"/>
-							</div>
-							<div class="form-group">
-								<label>Address</label>
-								<input type="text" name="address" class="form-control" required="required"/>
-							</div>
-						</div>	
-					</div>
-					<div style="clear:both;"></div>
-					<div class="modal-footer">
-						<button class="btn btn-primary" name="save"><span class="glyphicon glyphicon-save"></span> Save</button>
-						<button type="button" class="btn btn-danger" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Close</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-<script src="js/jquery-3.2.1.min.js"></script>
-<script src="js/bootstrap.js"></script>
 </body>
 </html>
