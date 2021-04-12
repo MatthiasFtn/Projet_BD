953ee9fb3c31452b2c0ed4e99452ecdbacf13345
Matthias <mattfon23@gmail.com>
2021-04-12 21:17:26 +0200

    update

--- Croisiere_BD/Tables/Triggers.sql
+++ Croisiere_BD/Tables/Triggers.sql
@@ -5,3 +5,24 @@ pragma foreign_keys=true;
 .headers on
 
 
+-- DROP TRIGGER create_reservation;
+
+
+-- CREATE TRIGGER create_reservation
+--     AFTER INSERT ON Reservations
+-- BEGIN
+
+--     UPDATE Clients SET id_rang = $id_rang WHERE id_rang = null
+
+--     INSERT INTO Client
+--     SET nom = new.nom,
+--         prenom = new.prenom,
+--         age = new.age,
+--         sexe = new.sexe,
+--         id_rang = new.id_rang;
+        
+--     INSERT INTO Factures
+--     SET date_facture = new.date_facture,
+--         h_facture = new.h_facture,
+--         id_client = new.id_client;
+-- END;
